72c44da0ae002777903c5ee60238dfa0ccc32d04
rgabuat <johncarlgabuat@gmail.com>
2022-09-25 23:35:28 +0800

    main_menu

--- app/src/main/java/com/example/petnership_kairos/MainActivity.java
+++ app/src/main/java/com/example/petnership_kairos/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.petnership_kairos;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setTheme(R.style.Theme_Petnership_Kairos);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drawer);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.menu_open, R.string.menu_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+//            {
+//                switch (item.getItemId())
+//                {
+//                    case R.id.nav_home:
+//                        Log.i("MENU_DRAWER_TAG","Home item is clicked");
+//                        drawerLayout.closeDrawer(GravityCompat.START);
+//                        break;
+//                }
+//
+//                return true;
+//            }
+//        });
     }
 }
