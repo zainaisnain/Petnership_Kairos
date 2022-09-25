622694296e7a7ef5750a549c362c82fd5cb47e8c
lacbainnel <j.lacbain@yahoo.com>
2022-09-25 12:44:20 +0800

    act-dashboard-to-pet-profile

--- app/src/main/java/com/example/petnership_kairos/MainActivity.java
+++ app/src/main/java/com/example/petnership_kairos/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.petnership_kairos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public CardView card1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        card1 = (CardView) findViewById(R.id.dogs);
+//        card1 = (CardView) findViewById(R.id.dogs);
+
+        card1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()) {
+            case R.id.dogs:
+                i = new Intent(this, PetProfile.class);
+                startActivity(i);
+                break;
+
+            default:
+                break;
+        }
     }
 }
