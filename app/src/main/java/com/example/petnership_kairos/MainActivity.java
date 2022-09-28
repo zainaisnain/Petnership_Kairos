3d6e6835c92f6dd032c01b11645db2481be35778
lacbainnel <j.lacbain@yahoo.com>
2022-09-28 19:19:08 +0800

    pet-profile-complete

--- app/src/main/java/com/example/petnership_kairos/MainActivity.java
+++ app/src/main/java/com/example/petnership_kairos/MainActivity.java
@@ -7,9 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public CardView card1;
+    public FloatingActionButton b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +21,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         card1 = (CardView) findViewById(R.id.dogs);
-//        card1 = (CardView) findViewById(R.id.dogs);
+        b1 = (FloatingActionButton) findViewById(R.id.fabAdd);
 
         card1.setOnClickListener(this);
+        b1.setOnClickListener(this);
     }
 
     @Override
@@ -30,6 +34,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.dogs:
                 i = new Intent(this, activity_userprofile.class);
+                startActivity(i);
+                break;
+
+            case R.id.fabAdd:
+                i = new Intent(this, activity_petprofilequestionnaire.class);
                 startActivity(i);
                 break;
 
