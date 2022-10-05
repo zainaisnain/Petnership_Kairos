f21894541ed24930e31f53bec40929b97a232de1
rvtlbonus <201910599@fit.edu.ph>
2022-10-05 23:57:47 +0800

    BONUS Forgot Password

---
+++ app/src/main/java/com/example/petnership_kairos/ForgotPassword3.java
@@ -0,0 +1,14 @@
+package com.example.petnership_kairos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPassword3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password3);
+    }
+}
