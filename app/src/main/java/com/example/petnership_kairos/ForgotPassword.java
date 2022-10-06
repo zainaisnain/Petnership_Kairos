5bd88a757e63676a651700df8566d6c28175b3c0
Zaina Isnain <zainaisnain@gmail.com>
2022-10-06 09:20:26 +0800

    Added BE to Forgot Password

--- app/src/main/java/com/example/petnership_kairos/ForgotPassword.java
+++ app/src/main/java/com/example/petnership_kairos/ForgotPassword.java
@@ -1,9 +1,7 @@
 package com.example.petnership_kairos;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +15,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private EditText emailForgot;
     private Button proceedForgot;
     private String email;
@@ -27,25 +25,27 @@ public class ForgotPassword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        emailForgot = findViewById(R.id.loginusername);
-        email = emailForgot.getText().toString().trim();
-
+        proceedForgot = findViewById(R.id.forgot_proceed);
         proceedForgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.sendPasswordResetEmail(email)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()){
-                                    Toast.makeText(ForgotPassword.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(ForgotPassword.this, ForgotPassword4.class));
-                                }
-                            }
-                        });
+                sendPasswordReset();
             }
         });
     }
 
+    private void sendPasswordReset(){
+        emailForgot = findViewById(R.id.loginusername);
+        email = emailForgot.getText().toString().trim();
 
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(ForgotPassword.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
