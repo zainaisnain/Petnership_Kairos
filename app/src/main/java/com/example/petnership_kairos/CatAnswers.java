0629254cf900331d1e36e5c3d84dc130d917913a
Zaina Isnain <zainaisnain@gmail.com>
2022-10-16 14:38:05 +0800

    added pet status

--- app/src/main/java/com/example/petnership_kairos/CatAnswers.java
+++ app/src/main/java/com/example/petnership_kairos/CatAnswers.java
@@ -2,15 +2,16 @@ package com.example.petnership_kairos;
 
 public class CatAnswers {
     public int q1,q2,q3,q4,q5,q6,q7, q8;
-    public String shelter, petName, petAge, petSex, petDesc, imageName, petID, q9;
+    public String shelter, petName, petAge, petSex, petStatus, petDesc, imageName, petID, q9;
 
     public CatAnswers(){}
 
-    public CatAnswers(String shelter, String petName, String petAge, String petSex, String petDesc, String imageName, String petID, int q1, int q2, int q3, int q4, int q5, int q6, int q7, int q8, String q9){
+    public CatAnswers(String shelter, String petName, String petAge, String petSex, String petStatus, String petDesc, String imageName, String petID, int q1, int q2, int q3, int q4, int q5, int q6, int q7, int q8, String q9){
         this.shelter=shelter;
         this.petName = petName;
         this.petAge = petAge;
         this.petSex = petSex;
+        this.petStatus = petStatus;
         this.petDesc = petDesc;
         this.imageName = imageName;
         this.petID = petID;
