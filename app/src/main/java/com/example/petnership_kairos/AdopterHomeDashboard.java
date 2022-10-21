2850590833fb796eb67699da50dc6335f6c44d2e
rvtlbonus <201910599@fit.edu.ph>
2022-10-21 13:11:34 +0800

    Some changes again

--- app/src/main/java/com/example/petnership_kairos/AdopterHomeDashboard.java
+++ app/src/main/java/com/example/petnership_kairos/AdopterHomeDashboard.java
@@ -86,10 +86,7 @@ public class AdopterHomeDashboard<cvApplicationHistory> extends Fragment {
         cvAdopterInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-                ShelterListOfDogsFragment PetDogs = new ShelterListOfDogsFragment();
-                transaction.replace(R.id.nav_host_fragment,PetDogs);
-                transaction.commit();
+                startActivity(new Intent(getActivity(), AdopterEdit.class));
             }
         });
 
