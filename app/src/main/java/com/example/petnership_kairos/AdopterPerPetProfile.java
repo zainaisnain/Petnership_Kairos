fcf134d2d777132b66fdcfd9a810112deb9ae57a
Zaina Isnain <zainaisnain@gmail.com>
2022-10-25 08:23:03 +0800

    incomplete adopterperpetprofile. with errors

--- app/src/main/java/com/example/petnership_kairos/AdopterPerPetProfile.java
+++ app/src/main/java/com/example/petnership_kairos/AdopterPerPetProfile.java
@@ -1,28 +1,23 @@
 package com.example.petnership_kairos;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-public class AdopterPerPetProfile extends AppCompatActivity {
-
+public class AdopterPerPetProfile extends Fragment {
+    View view;
     protected static String petID, petImageName, petName, petBreed, petAge, petSex, petDescription;
 
     private TextView tvPetTitle, tvPetName, tvPetBreed, tvPetAge, tvPetSex, tvPetDescription;
@@ -31,70 +26,41 @@ public class AdopterPerPetProfile extends AppCompatActivity {
 
     DatabaseReference allPetsDBRef = FirebaseDatabase.getInstance().getReference().child("Pets").child("AllPets");
     StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    public AdopterPerPetProfile newInstance() {return new AdopterPerPetProfile();}
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adopter_per_pet_profile);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_adopter_per_pet_profile, container, false);
+        petID = view.getIntent().getStringExtra("PetID");
+        ivPetImage = view.findViewById(R.id.adopter_per_pet_image);
+        tvPetTitle = view.findViewById(R.id.adopter_per_pet_name_title);
+        tvPetName = view.findViewById(R.id.adopter_per_pet_name);
+        tvPetBreed = view.findViewById(R.id.adopter_per_pet_breed);
+        tvPetAge = view.findViewById(R.id.adopter_per_pet_age);
+        tvPetSex = view.findViewById(R.id.adopter_per_pet_sex);
+        tvPetDescription = view.findViewById(R.id.adopter_per_pet_description);
 
-        petID = getIntent().getStringExtra("PetID");
-        ivPetImage = findViewById(R.id.adopter_per_pet_image);
-        tvPetTitle = findViewById(R.id.adopter_per_pet_name_title);
-        tvPetName = findViewById(R.id.adopter_per_pet_name);
-        tvPetBreed = findViewById(R.id.adopter_per_pet_breed);
-        tvPetAge = findViewById(R.id.adopter_per_pet_age);
-        tvPetSex = findViewById(R.id.adopter_per_pet_sex);
-        tvPetDescription = findViewById(R.id.adopter_per_pet_description);
-
-        backBtn = findViewById(R.id.adopter_per_pet_back_btn);
+        backBtn = view.findViewById(R.id.adopter_per_pet_back_btn);
 
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(AdopterPerPetProfile.this, BrowseAnimals.class));
+//                startActivity(new Intent(AdopterPerPetProfileAct.this, BrowseAnimals.class));
             }
         });
 
         setUpPetImage();
+        return view;
     }
 
-    private void setUpPetImage() {
-        allPetsDBRef.child(petID).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                petImageName = (String) snapshot.child("imageName").getValue();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-                storageReference.child("Pets/").child(petImageName).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Glide.with(AdopterPerPetProfile.this).load(uri.toString()).into(ivPetImage);
-                    }
-                });
-
-                petName = (String) snapshot.child("petName").getValue();
-                petBreed = (String) snapshot.child("q10").getValue();
-                petAge = (String) snapshot.child("petAge").getValue();
-                petSex = (String) snapshot.child("petSex").getValue();
-                petDescription = (String) snapshot.child("petDesc").getValue();
-
-                String petType = String.valueOf(snapshot.child(petID).child("petType").getValue());
-                if(petType.equals("dog")){
-                    petBreed = String.valueOf(snapshot.child(petID).child("q10").getValue());
-                }else if(petType.equals("cat")){
-                    petBreed = String.valueOf(snapshot.child(petID).child("q9").getValue());
-                }
-
-                tvPetTitle.setText(petName + "'s Profile");
-                tvPetName.setText(petName);
-                tvPetBreed.setText(petBreed);
-                tvPetAge.setText(petAge);
-                tvPetSex.setText(petSex);
-                tvPetDescription.setText(petDescription);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
     }
+
 }
--- app/src/main/java/com/example/petnership_kairos/BrowseAnimalsAdapter.java
+++ app/src/main/java/com/example/petnership_kairos/BrowseAnimalsAdapter.java
@@ -1,6 +1,5 @@
 package com.example.petnership_kairos;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -8,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -60,7 +58,7 @@ public class BrowseAnimalsAdapter extends RecyclerView.Adapter<BrowseAnimalsAdap
         holder.cvAnimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), AdopterPerPetProfile.class);
+                Intent intent = new Intent(view.getContext(), AdopterPerPetProfileAct.class);
                 intent.putExtra("PetID", registeredPetDataList.getPetID());
                 view.getContext().startActivity(intent);
             }
