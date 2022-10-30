3f0dfd44676385952d2f12795c296f290e5cd8be
kvatienza <kryschchaliaatienza@gmail.com>
2022-10-30 16:01:16 +0800

    Pop-up UI changes

--- app/src/main/java/com/example/petnership_kairos/HelpPopup.java
+++ app/src/main/java/com/example/petnership_kairos/HelpPopup.java
@@ -4,10 +4,15 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -17,6 +22,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Objects;
+
 public class HelpPopup extends DialogFragment {
     public View view;
     ImageButton btnOk;
@@ -25,9 +33,30 @@ public class HelpPopup extends DialogFragment {
     public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         view = inflater.inflate(R.layout.fragment_help_popup, container, false);
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        }
 
         btnOk = view.findViewById(R.id.closeBTN);
         btnOk.setOnClickListener(view -> HelpPopup.this.dismiss());
+
+        Objects.requireNonNull(getDialog()).setCanceledOnTouchOutside(true);
+
+        Window window = getDialog().getWindow();
+
+        // set "origin" to top left corner, so to speak
+        window.setGravity(Gravity.TOP|Gravity.LEFT);
+
+        // after that, setting values for x and y works "naturally"
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.x = 100;
+        params.y = 350;
+        window.setAttributes(params);
+
+
+
+
         return view;
     }
 }
