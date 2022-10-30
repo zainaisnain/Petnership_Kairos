dd1932e0f6660ff3034ba650cc8cc1ac3db61776
Zaina Isnain <zainaisnain@gmail.com>
2022-10-30 12:09:30 +0800

    minor fixes

--- app/src/main/java/com/example/petnership_kairos/StartOfQuestionnaire.java
+++ app/src/main/java/com/example/petnership_kairos/StartOfQuestionnaire.java
@@ -3,29 +3,18 @@ package com.example.petnership_kairos;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import android.content.Intent;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.FragmentTransaction;
 
 public class StartOfQuestionnaire extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -36,22 +25,19 @@ public class StartOfQuestionnaire extends AppCompatActivity implements Navigatio
     NavigationView navigationView;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
-    Button logout, cancelLogout;
-    Dialog dialog;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.shelter_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser currUser = auth.getCurrentUser();
 
 //        if(currUser == null)
 //        {
@@ -61,14 +47,13 @@ public class StartOfQuestionnaire extends AppCompatActivity implements Navigatio
 //            return;
 //        }
 
-
-
         /**
-         * Set home fragment as default page
+         * Set Questionnaire Welcoem page fragment as default page
          */
         if(savedInstanceState == null) {
-            getSupportFragmentManager().
-                    beginTransaction().replace(R.id.nav_host_fragment,new ShelterHomeDashboard()).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                    new FragmentQuestionnaireWelcome()).addToBackStack("questionnaireHomeFragment").commit();
+
         }
 
         /**
@@ -81,7 +66,7 @@ public class StartOfQuestionnaire extends AppCompatActivity implements Navigatio
         /**
          * Tool bar
          */
-        // setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
         /**
          * Navigation Drawer Menu
@@ -93,6 +78,7 @@ public class StartOfQuestionnaire extends AppCompatActivity implements Navigatio
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
     }
+
 
 
     @Override
@@ -123,31 +109,21 @@ public class StartOfQuestionnaire extends AppCompatActivity implements Navigatio
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId())
         {
-
-            case R.id.nav_reg_pets:
-                FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
-                ShelterRegisterPets shelterRegisterPets = new ShelterRegisterPets();
-                transaction1.replace(R.id.shelterDashboardFrag, shelterRegisterPets);
-                transaction1.commit();
-//                getSupportFragmentManager().beginTransaction().replace(R.id.shelter_dashboard_frag,
-//                        new ShelterRegisterPets()).commit();
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new home()).commit();
                 break;
 
             case R.id.nav_reg_my_pets:
-                getSupportFragmentManager().beginTransaction().replace(R.id.shelterDashboardFrag,
-                        new ShelterListOfPetsFragment()).commit();
-                break;
-
-            case R.id.nav_shelter_edit_info:
-                startActivity(new Intent(StartOfQuestionnaire.this, ShelterEditInfo.class));
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new PetProfileDogs()).commit();
                 break;
 
             case R.id.nav_logout:
-//                startActivity(new Intent(ShelterDashboard.this, MyLogoutDialog.class));
                 userLogout();
-//                getSupportFragmentManager().beginTransaction().replace(R.id.shelter_dashboard_frag,
-//                        new MyLogoutDialog()).commit();
                 break;
+
+
         }
 
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -156,50 +132,10 @@ public class StartOfQuestionnaire extends AppCompatActivity implements Navigatio
 
     private void userLogout()
     {
-//        FirebaseAuth.getInstance().signOut();
-//        Intent intent = new Intent(ShelterDashboard.this,LoginActivity.class);
-//        startActivity(intent);
-//        finish();
-        dialog = new Dialog(StartOfQuestionnaire.this);
-        dialog.setContentView(R.layout.logout_dialog);
-        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialog.setCancelable(false);
-        dialog.getWindow().getAttributes().windowAnimations = R.style.animation;
-
-        logout = dialog.findViewById(R.id.buttonOk);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(StartOfQuestionnaire.this,LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        cancelLogout = dialog.findViewById(R.id.buttonCancel);
-        cancelLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
-
-//    @Override
-//    public void onClick(View view) {
-//        Intent i;
-//
-//        switch (view.getId()) {
-//            case R.id.dogs:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
-//                        new register()).commit();
-//                break;
-//
-//            default:
-//                break;
-//        }
-//    }
 }
