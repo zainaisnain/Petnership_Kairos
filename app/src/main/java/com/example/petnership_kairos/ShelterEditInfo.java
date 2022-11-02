03aee16ef4010e339abf92bf11db59eb2f4eb240
rvtlbonus <201910599@fit.edu.ph>
2022-11-02 20:15:55 +0800

    Fixed 'prefers' and app crashing despite successfully adding to DBS

--- app/src/main/java/com/example/petnership_kairos/ShelterEditInfo.java
+++ app/src/main/java/com/example/petnership_kairos/ShelterEditInfo.java
@@ -242,7 +242,9 @@ public class ShelterEditInfo extends AppCompatActivity {
             public void onClick(View arg0) {
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 ShelterHomeDashboard shelterHomeDashboard = new ShelterHomeDashboard();
-                fragmentTransaction.replace(R.id.sheltereditInfo, shelterHomeDashboard).commit();
+                fragmentTransaction.replace(R.id.sheltereditInfo, shelterHomeDashboard);
+                fragmentTransaction.addToBackStack("shelterEditInfo");
+                fragmentTransaction.commit();
                 saveDialog.dismiss();
 
             }
